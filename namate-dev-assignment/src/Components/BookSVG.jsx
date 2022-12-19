b8c90d78bae44f6ea5972a0873f2f188ed5616e5
DESKTOP-65CV4VO\Acer <shivamdamre913@gmail.com>
2022-12-19 08:45:30 +0530

    created the theme context as well as lazy loaded the application

---
+++ namate-dev-assignment/src/Components/BookSVG.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SvgComponent = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130 174" {...props}>
+    <path
+      style={{
+        blockProgression: 'tb',
+        textIndent: 0,
+        color: '#000',
+        textTransform: 'none'
+      }}
+      d="M249.29 502.51v174.003h130V512.997h-18.84V502.51zm111.16 19.994h9.42v144.5H258.71v-9.536h101.74zm-88.08 9.477h65.942v19.013H272.37z"
+      transform="translate(-249.29 -502.51)"
+    />
+  </svg>
+);
+
+export default SvgComponent;
--- namate-dev-assignment/src/Components/Card.jsx
+++ namate-dev-assignment/src/Components/Card.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import LinkedinIcon from '../../public/assets/linkedin-icon.png';
 import GithubIcon from '../../public/assets/github-icon.png';
+import { ThemeContext } from '../Contexts/ThemeContextProvider';
+import { Link } from 'react-router-dom';
 
 const Card = ({ info }) => {
-  const { name, company, location, bio, linkedin, html_url: url, avatar_url: avatar } = info;
+  const { name, company, location, bio, linkedin, html_url: url, avatar_url: avatar, login } = info;
+  const [theme] = useContext(ThemeContext);
   return (
-    <div className="card">
-      <img className="card-img" src={avatar} alt="avatar_url" />
+    <div className={`card ${theme === 'Light' ? 'card-light' : 'card-dark'}`}>
+      <Link
+        style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center' }}
+        to={`/mate/${login}`}>
+        <img loading="lazy" className="card-img" src={avatar} alt="avatar_url" />
+      </Link>
       <div className="card-content">
         <hr className="hr" />
         <div className="card-name">
-          {name + ' '}
+          {(name ?? 'John Doe') + ', '}
           <span>{company || ''}</span>
         </div>
         <div className="card-address">{location}</div>
         <div>{bio}</div>
         <div className="card-icons">
           <a href={linkedin} target="_blank" rel="noreferrer">
-            <img width={30} height={30} src={LinkedinIcon} alt="linkedin-icon" />
+            <img loading="lazy" width={30} height={30} src={LinkedinIcon} alt="linkedin-icon" />
           </a>
           <a href={url} target="_blank" rel="noreferrer">
-            <img width={30} height={30} src={GithubIcon} alt="github-icon" />
+            <img loading="lazy" width={30} height={30} src={GithubIcon} alt="github-icon" />
           </a>
         </div>
       </div>
